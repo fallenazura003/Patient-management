68809c8d6329bc6ffdee0ed2d06e6e6303e15ee5
fallenazura003 <fallenazura003@gmail.com>
2025-06-16 15:23:13 +0700

    Complete patient CRUD

--- patient-service/src/main/java/com/forsakenecho/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/forsakenecho/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.forsakenecho.patientservice.dto;
 
+import com.forsakenecho.patientservice.dto.Validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -18,7 +19,7 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of Birth is required")
     private String dateOfBirth;
 
-    @NotBlank(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
     private String registeredDate;
 
     public String getName() {
